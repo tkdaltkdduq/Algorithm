03854b76bf9213265b7daef5376ab254fd3bc7f5
seonghun <dev.seonghun@gmail.com>
2021-09-21 03:41:23 +0900

    seonghun add programmers 12921 using memories and checking exp

--- week2/Seonghun_Programmers_12921.cpp
+++ week2/Seonghun_Programmers_12921.cpp
@@ -4,24 +4,21 @@ using namespace std;
 
 int solution(int n) {
     int answer = 1;
+    bool IsprimeNum = true;
     vector<int> result;
     vector<int>::iterator iter;
     if(n == 1) return 0;
     for(int k=3;k<=n;){
-        for (iter = result.begin(); iter != result.end(); ++iter)
+        for (iter = result.begin(); iter != result.end() && (((*iter)*(*iter))<=k); ++iter)
         {
-            if(!(k%*iter)){break;}
+            if(!(k%*iter)){IsprimeNum=false; break;}
         }
-        if(iter == result.end()){
+        if(IsprimeNum){
             result.push_back(k);
         }
+        IsprimeNum=true;
         k=k+2;
     }
-    answer += result.size();
+    answer+= result.size();
     return answer;
-}
-
-
-int main(){
-    std::cout << solution(1000000);
 }
