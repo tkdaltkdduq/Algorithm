8b40366ff9899349b0e2552f64156fb91b743756
seonghun <dev.seonghun@gmail.com>
2021-09-20 22:34:38 +0900

    seonghun add codeup 1920

---
+++ week2/Seonghun_Codeup_1920.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+void changeToBinaryNum(int quo){
+    int div = quo % 2;
+    quo = quo / 2;
+    if(quo > 0){
+        changeToBinaryNum(quo);
+        std::cout << div;
+    }
+    else{
+        std::cout << div;
+        return;
+    }
+}
+
+int main() {
+    int num;
+    std::cin >> num;
+    changeToBinaryNum(num);
+    return 0;
+}
+
+
