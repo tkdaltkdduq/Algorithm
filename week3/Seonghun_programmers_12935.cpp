32264b8fd9430775a60de7a73c5442b0cb7257c2
seonghun <dev.seonghun@gmail.com>
2021-09-21 14:00:00 +0900

    seonghun add programmers 12935

---
+++ week3/Seonghun_programmers_12935.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    if(arr.size()-1 == 0){ arr.clear(); arr.push_back(-1); return arr;}
+    arr.erase( min_element(arr.begin(), arr.end()));
+    return arr;
+}
